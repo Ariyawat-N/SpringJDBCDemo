21bf75bc79519aff29ddddb6369d2dc16e9af7ab
ging <ariyawat.nky@gmail.com>
2024-02-06 14:34:28 +0700

    change users name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into alien (id,name,tech) values (101,'Mild','Blockchain');
-insert into alien (id,name,tech) values (102,'Mint','AI');
-insert into alien (id,name,tech) values (103,'Mawin','IoT');
+insert into alien (id,name,tech) values (101,'WOOP WOOP','Blockchain');
+insert into alien (id,name,tech) values (102,'PE PE','AI');
+insert into alien (id,name,tech) values (103,'ADAM','IoT');
