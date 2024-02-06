bd104b11effb369a5049ea0280659e860201eaad
ging <ariyawat.nky@gmail.com>
2024-02-06 14:32:08 +0700

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id,name,tech) values (101,'Mild','Blockchain');
+insert into alien (id,name,tech) values (102,'Mint','AI');
+insert into alien (id,name,tech) values (103,'Mawin','IoT');
